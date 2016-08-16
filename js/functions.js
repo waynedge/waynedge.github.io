bdbc8b612a32f56235d892a7619afe2beb3cb8bb
waynedge <waynedge@gmail.com>
2016-08-16 22:53:15 +0100

    initial files

---
+++ js/functions.js
@@ -0,0 +1,47 @@
+var maximumProperties = $(':checkbox').length + 1;
+var totalProperties   = 0;
+
+$(function(){
+    $('#bulkSlider').attr('type','range').attr('min','1').attr('max',maximumProperties).on('change', function(){
+        var currentSliderVal = $('#bulkSlider').val();
+        totalProperties = currentSliderVal;
+        $('#TotalProperties').prop('value',currentSliderVal);
+    });
+
+    $('#TotalProperties').val(totalProperties).blur(function(){
+        var currentSliderVal = $(this).val();
+        $('#bulkSlider').prop('value', currentSliderVal );
+    });
+});
+
+$('#bulkSlider').on('change', function(){
+    var currentSliderVal = $('#bulkSlider').val();
+    totalProperties = currentSliderVal;
+    $('#TotalProperties').prop('value',currentSliderVal);
+});
+
+/* Toggle checkbox */
+$('#selectAllSel').on('change', function () {
+    if (this.checked){
+        var n = totalProperties;
+            $(':checkbox').slice(0,n).each(function () {
+                $(this).prop('checked', true);
+            });
+    }else{
+            $(':checkbox').each(function () {
+                $(':checkbox').prop('checked', false);
+            });
+    }
+});
+
+$('#reassign').on('click', function () {
+        var amt = totalProperties;
+        var dpVal = $('#selectMasterPlanningPartner').val();
+        $('.currentAsignedLabel').slice(0,amt).each(function () {
+            $(this).html(dpVal);
+    });
+
+});
+$('span.glyphicon.glyphicon-trash').on('click',function () {
+   $(this).closest('tr').remove();
+});
